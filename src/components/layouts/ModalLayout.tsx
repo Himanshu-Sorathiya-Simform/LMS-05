eaeef10c2aa5e0b8249cf84e027bc68831b41baf
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-27 15:59:00 +0530

    add modal component for showing modals and forms

---
+++ src/components/layouts/ModalLayout.tsx
@@ -0,0 +1,33 @@
+import { forwardRef } from "react";
+import { createPortal } from "react-dom";
+import "./../../css/Modal.css";
+
+interface ModalProps {
+	onClose: () => void;
+	children: React.ReactNode;
+	className?: string;
+}
+
+const Modal = forwardRef<HTMLDialogElement, ModalProps>(
+	({ onClose, children, className = "" }, ref) => {
+		const handleCancel = (event: React.SyntheticEvent<HTMLDialogElement>) => {
+			event.preventDefault();
+			onClose();
+		};
+
+		return createPortal(
+			<dialog
+				ref={ref}
+				onCancel={handleCancel}
+				className={`universal-modal ${className}`}
+			>
+				<div className="modal-container">{children}</div>
+			</dialog>,
+			document.body,
+		);
+	},
+);
+
+Modal.displayName = "Modal";
+
+export default Modal;
